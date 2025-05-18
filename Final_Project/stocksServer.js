458a2a50ec26284d219fa231bd2c9f0b6cf67cde
MaiwandR <114307104+MaiwandR@users.noreply.github.com>
2025-05-18 15:44:15 -0400

    Changing setup for routes requirment

--- Final_Project/stocksServer.js
+++ Final_Project/stocksServer.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { authRouter, injectAuthDB } = require('./routes/authRoutes');
 const { MongoClient, ObjectId } = require('mongodb');
 const session = require('express-session');
 const bcrypt = require('bcrypt');
@@ -23,6 +24,7 @@ app.use(session({
 app.set('view engine', 'ejs'); // ✅ add this
 app.set('views', './templates'); // ✅ add this too
 
+  
 
 const PORT = 3000;
 
@@ -35,6 +37,8 @@ const STAR_URL = `https://www.alphavantage.co/query?function=TOP_GAINERS_LOSERS&
 MongoClient.connect(process.env.DB_CONNECT_STRING)
   .then(client => {
     db = client.db(process.env.DB_NAME);
+    injectAuthDB(db); // give db to auth routes
+    app.use(authRouter); // use auth routes
     app.listen(PORT, () => {
       console.log(`Server is running on: http://127.0.0.1:${PORT}`);
     });
@@ -64,6 +68,7 @@ app.get('/', (req, res) => {
         update,
         losers,
         actives,
+        logginIn: req.session.userId ? true : false,
        }); // ✅ render 'index', not 'index.ejs'
     })
     .catch(error => {
@@ -80,7 +85,8 @@ app.get('/', (req, res) => {
             { ticker: 'AMZN', price: 125.00, volume: 8500000 },
             { ticker: 'NVDA', price: 480.00, volume: 7200000 }
           ],
-          update: '2025-05-08 16:00:00'
+          update: '2025-05-08 16:00:00',
+          logginIn: false,
         });
       });
 });
@@ -120,6 +126,7 @@ app.get('/search/:name', (req, res) => {
                 name,
                 update,
                 lastTen,
+                logginIn: req.session.userId ? true : false,
             })
 
         })
@@ -141,85 +148,130 @@ app.get('/search/:name', (req, res) => {
             res.render('detail', {
               name: req.params.name || 'AAPL',
               update: '2025-05-08',
-              lastTen
+              lastTen,
+              logginIn: req.session.userId ? true : false,
             });
           });
 });
 
+
+// The first register renders the register.ejs file
+// The second one is used to register the user
+// We hash the password using bcrypt and store it in the database
+// We also set the current user ID in the session from the database
+
+// app.get('/register', (req, res) => {
+//     res.render('register', { error: null });
+// });
+
+// app.post('/register', async (req, res) => {
+//   const username = req.body.username.trim();
+//   const password = req.body.password.trim();   
+//   const email = req.body.email.trim(); 
+//     const hashedPassword = await bcrypt.hash(password, 10);
+
+//     const existingUser = await db.collection('userStocks').findOne({ username });
+//     error = [];
+//     if (!username || !password || !email) {
+//         error.push('All fields are required');
+//         return res.render('register', { error: error});
+//     }
+
+//     if (existingUser) {
+//          error.push('Username already exists');
+//     }
+//     const existingEmail = await db.collection('userStocks').findOne({ email });
+//     if (existingEmail) {
+//          error.push('Email already exists');
+//     }
+//     if (password.length < 6) {
+//          error.push('Password must be at least 6 characters long');
+//     }
+//     if (error.length > 0) {
+//         return res.render('register', { error });
+
+//     }
+
+//     const user = await db.collection('userStocks').insertOne({
+//         username,
+//         email,
+//         password: hashedPassword,
+//         watchlist: [],
+//         bought: [],
+//         sold: [],
+//     });
+//     req.session.userId = user.insertedId;
+//     res.redirect('/login');
+// } );
+
+// // The first login renders the login.ejs file
+// // The second one is used to log the user in
+
+// // We set the currnt user ID in the sesison form the database
+
+// app.get('/login', (req, res) => {
+//     const error = req.query.error || null;
+//     res.render('login', {error});
+// });
+
+// app.post('/login', async (req, res) => {
+//   const username = req.body.username.trim();
+//   const password = req.body.password.trim();    
+//   const user = await db.collection('userStocks').findOne({username});
+//     if (user && await bcrypt.compare(password, user.password)) {
+//         req.session.userId = user._id;
+//         res.redirect('/dashboard');
+//     } else{
+//         const error = 'Invalid username or password';
+//         res.render('login', {error});
+//     }
+// });
+
+// // The logout path is used to log the user out
+// // We destroy the session and redirect to the index page
+
+// app.get('/logout', (req, res) => {
+//     req.session.destroy();
+//     res.redirect('/');
+// });
+
+// Buy funtionality 
+// User buy stock when they vist the detail page for a stock
+// Then they fill out a form that asks them how many share they want to buy
 app.get('/buy', async (req, res) => {
-    const userId = req.session.userId;
-    if (!userId) {
-        return res.redirect('/login');
-    }
-    const user = await db.collection('userStocks').findOne({ _id: new ObjectId(userId) });
-        const ticker = req.query.ticker || ''; 
-        const price = req.query.price || '';
-        const message = req.session.message || null;
-      delete req.session.message;
-    res.render('buy', { user,ticker, price, message });
+  const userId = req.session.userId;
+  if (!userId) {
+      return res.redirect('/login');
+  }
+  const user = await db.collection('userStocks').findOne({ _id: new ObjectId(userId) });
+      const ticker = req.query.ticker || ''; 
+      const price = req.query.price || '';
+      const message = req.session.message || null;
+    delete req.session.message;
+  res.render('buy', { user,ticker, price, message });
 });
 
 app.post('/buy', async (req, res) => {
-    const { ticker, price, amount } = req.body;
-    const parseamount = parseFloat(amount);
-    const parsedPrice = parseFloat(price);
-    const total = parseamount * parsedPrice; 
-    const userId = req.session.userId;
-    const user = await db.collection('userStocks').findOne({ _id: new ObjectId(userId) });
-    const bought = user.bought || [];
-    bought.push({ ticker, price: parsedPrice, amount: parseamount, total });
-    await db.collection('userStocks').updateOne(
-      { _id: new ObjectId(userId) },
-       { $set: { bought } });
-       const message = `Successfully purchased ${parseamount} share(s) of ${ticker} at $${parsedPrice.toFixed(2)} each.`;
-    req.session.message = message;
-    res.redirect('/buy');
+  const { ticker, price, amount } = req.body;
+  const parseamount = parseFloat(amount);
+  const parsedPrice = parseFloat(price);
+  const total = parseamount * parsedPrice; 
+  const userId = req.session.userId;
+  const user = await db.collection('userStocks').findOne({ _id: new ObjectId(userId) });
+  const bought = user.bought || [];
+  bought.push({ ticker, price: parsedPrice, amount: parseamount, total });
+  await db.collection('userStocks').updateOne(
+    { _id: new ObjectId(userId) },
+     { $set: { bought } });
+     const message = `Successfully purchased ${parseamount} share(s) of ${ticker} at $${parsedPrice.toFixed(2)} each.`;
+  req.session.message = message;
+  res.redirect('/buy');
 });
 
-app.get('/dashboard', async (req, res) => {
-  if (!req.session.userId) {
-    return res.redirect('/login');
-  }
-
-  try {
-    const user = await db.collection(process.env.MONGO_COLLECTION).findOne({ _id: new ObjectId(req.session.userId) });
-
-    const bought = user.bought || [];
-const sold = user.sold || [];
-
-const ownedStocks = {};
-
-// Aggregate bought
-bought.forEach(txn => {
-  if (!ownedStocks[txn.ticker]) {
-    ownedStocks[txn.ticker] = 0;
-  }
-  ownedStocks[txn.ticker] += txn.amount;
-});
-
-// Subtract sold
-sold.forEach(txn => {
-  if (!ownedStocks[txn.ticker]) {
-    ownedStocks[txn.ticker] = 0;
-  }
-  ownedStocks[txn.ticker] -= txn.amount;
-});
-
-// Remove stocks with 0 or less shares
-Object.keys(ownedStocks).forEach(ticker => {
-  if (ownedStocks[ticker] <= 0) {
-    delete ownedStocks[ticker];
-  }
-});
-
-   res.render('dashboard', { user, ownedStocks });
-  } catch (err) {
-    console.error('Error fetching user stocks:', err);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-
+// Sell functionality
+// Makes the proper checks before makeing any sale or changes to user data
+// It checks if the user has the stock in their bought list and if they have enough shares to sell
+// If they do we update the bought list and add the stock to the sold list
 app.get('/sell', async (req, res) => {
   const userId = req.session.userId;
   if (!userId) return res.redirect('/login');
@@ -273,88 +325,50 @@ app.post('/sell', async (req, res) => {
   res.redirect(`/sell?ticker=${ticker}&price=${price}`);
 });
 
-// The first register renders the register.ejs file
-// The second one is used to register the user
-// We hash the password using bcrypt and store it in the database
-// We also set the current user ID in the session from the database
 
-app.get('/register', (req, res) => {
-    res.render('register', { error: null });
+// User Dashboard which will help users see stocks transactions and more information about theri portfolio
+// The dashboard will show the bought and sold stocks
+app.get('/dashboard', async (req, res) => {
+  if (!req.session.userId) {
+    return res.redirect('/login');
+  }
+
+  try {
+    const user = await db.collection(process.env.MONGO_COLLECTION).findOne({ _id: new ObjectId(req.session.userId) });
+
+    const bought = user.bought || [];
+const sold = user.sold || [];
+
+const ownedStocks = {};
+
+// Aggregate bought
+bought.forEach(txn => {
+  if (!ownedStocks[txn.ticker]) {
+    ownedStocks[txn.ticker] = 0;
+  }
+  ownedStocks[txn.ticker] += txn.amount;
 });
 
-app.post('/register', async (req, res) => {
-  const username = req.body.username.trim();
-  const password = req.body.password.trim();   
-  const email = req.body.email.trim(); 
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const existingUser = await db.collection('userStocks').findOne({ username });
-    error = [];
-    if (!username || !password || !email) {
-        error.push('All fields are required');
-        return res.render('register', { error: error});
-    }
-
-    if (existingUser) {
-         error.push('Username already exists');
-    }
-    const existingEmail = await db.collection('userStocks').findOne({ email });
-    if (existingEmail) {
-         error.push('Email already exists');
-    }
-    if (password.length < 6) {
-         error.push('Password must be at least 6 characters long');
-    }
-    if (error.length > 0) {
-        return res.render('register', { error });
-
-    }
-    const user = await db.collection('userStocks').insertOne({
-        username,
-        email,
-        password: hashedPassword,
-        watchlist: [],
-        bought: [],
-        sold: [],
-    });
-    req.session.userId = user.insertedId;
-    res.redirect('/login');
-} );
-
-// The first login renders the login.ejs file
-// The second one is used to log the user in
-
-// We set the currnt user ID in the sesison form the database
-
-app.get('/login', (req, res) => {
-    const error = req.query.error || null;
-    res.render('login', {error});
+// Subtract sold
+sold.forEach(txn => {
+  if (!ownedStocks[txn.ticker]) {
+    ownedStocks[txn.ticker] = 0;
+  }
+  ownedStocks[txn.ticker] -= txn.amount;
 });
 
-app.post('/login', async (req, res) => {
-    const username = req.body.username.trim();
-      const password = req.body.password.trim();    
-      const user = await db.collection('userStocks').findOne({username});
-    if (!user) {
-        const error = 'Invalid username or password';
-        return res.render('login', { error });
-    }
-
-    const passwordMatches = await bcrypt.compare(password, user.password);
-
-    if (passwordMatches) {
-        req.session.userId = user._id;
-        res.redirect('/');
-    } else{
-        const error = 'Invalid username or password';
-        res.render('login', {error});
-    }
+// Remove stocks with 0 or less shares
+Object.keys(ownedStocks).forEach(ticker => {
+  if (ownedStocks[ticker] <= 0) {
+    delete ownedStocks[ticker];
+  }
 });
 
-// The logout path is used to log the user out
-// We destroy the session and redirect to the index page
-
-app.get('/logout', (req, res) => {
-    req.session.destroy();
-    res.redirect('/');
+   res.render('dashboard', { user, ownedStocks });
+  } catch (err) {
+    console.error('Error fetching user stocks:', err);
+    res.status(500).send('Internal Server Error');
+  }
 });
+
 
